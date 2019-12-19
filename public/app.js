3a186b2ee9d53ed7b3809300057fed1962ecd549
Cesar Gomez <zootechdrum@gmail.com>
2019-12-18 21:11:34 -0800

    added alerts when fields are missing

--- public/app.js
+++ public/app.js
@@ -13,10 +13,10 @@ $(document).ready(function(){
         e.preventDefault()
         $("form :input[type=radio]:checked").each(function() {
             var value = $(this).val();
-            data.answer.push(this.value)
+            data.answer.push(parseInt(this.value))
         });
         if(data.answer.length < 2){
-            console.log("not all boxes have been checked")
+            $("#alert").append("<p class='alert alert-danger'>Please answer every question</p>")
         }else if( $("#fName").val() === ''){
             console.log("First name is wrong")
         }else if($("#imageUrl").val() === ''){
