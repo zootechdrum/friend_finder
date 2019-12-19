344dcabb8b692ee8400f2f31064f8bd24ce9df0c
Cesar Gomez <zootechdrum@gmail.com>
2019-12-18 21:56:06 -0800

    added logic and error message if fields are left blank

--- public/app.js
+++ public/app.js
@@ -1,34 +1,40 @@
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     var data = {
-        name :'',
-        image : '',
+        name: '',
+        image: '',
         answer: []
     }
 
 
-    $(".form-button").click(function(e){
+    $(".form-button").click(function (e) {
         e.preventDefault()
-        $("form :input[type=radio]:checked").each(function() {
+        $("form :input[type=radio]:checked").each(function () {
             var value = $(this).val();
             data.answer.push(parseInt(this.value))
         });
-        if(data.answer.length < 2){
-            $("#alert").append("<p class='alert alert-danger'>Please answer every question</p>")
-        }else if( $("#fName").val() === ''){
-            console.log("First name is wrong")
-        }else if($("#imageUrl").val() === ''){
-            console.log("Missing iamge url")
-        }
-        
-        })
 
-    $.post("api/friends",name ,function(data) {
-            console.log(data)
+        $("#alert").empty();
+
+        if (data.answer.length < 2) {
+            $("#alert").append("<p class='alert alert-danger'>Please answer every question</p>")
+        } else if ($("#fName").val() === '') {
+            $("#alert").append("<p class='alert alert-danger'>Please Enter your name</p>")
+        } else if ($("#imageUrl").val() === '') {
+            $("#alert").append("<p class='alert alert-danger'>Please Enter your ImageUrl</p>")
+        } else {
+            data.name = $("#fName").val()
+            data.image = $("#imageUrl").val()
+            $.post("api/friends", data, function (data) {
+                console.log(data)
+            })
         }
-    )
+
+    })
+
+
 });
 
 
